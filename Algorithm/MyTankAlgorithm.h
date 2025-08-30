571d7f8549972b96093f42cbe8d7da23bd8f589a
Yaar Koren <forestkoren@gmail.com>
2025-08-30 14:28:18 +0300

    change MyTankAlgorithm to be the HunterAlgo (instead of holding a pointer to it)

--- Algorithm/MyTankAlgorithm.h
+++ Algorithm/MyTankAlgorithm.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <memory>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <optional>
 
 #include "../common/TankAlgorithm.h"
 #include "../common/ActionRequest.h"
@@ -8,16 +12,15 @@
 #include "../UserCommon/Position.h"
 #include "../UserCommon/Direction.h"
 #include "../UserCommon/Board.h"
+#include "../UserCommon/MyBattleInfo.h"
+
 
 namespace Algorithm_207177197_301251571 {
 
+//Hunter Algo
 class MyTankAlgorithm : public TankAlgorithm {
 public:
-    MyTankAlgorithm(std::unique_ptr<TankAlgorithm> actualAlgo,
-                    int playerIndex, int tankIndex);
-
     MyTankAlgorithm(int playerIndex, int tankIndex);
-
 
     ~MyTankAlgorithm() = default;
 
@@ -28,9 +31,33 @@ public:
     int getTankIndex() const { return tankIndex_; }
 
 private:
-    std::unique_ptr<TankAlgorithm> actualAlgo_;
     int playerIndex_;
     int tankIndex_;
+
+    std::optional<UserCommon_207177197_301251571::MyBattleInfo> currentInfo_;
+    std::vector<UserCommon_207177197_301251571::Position> currentPath;
+    UserCommon_207177197_301251571::Direction currentDirection_;
+    int turnsSinceLastUpdate_;
+    bool isDirectionInitialized_;
+    static constexpr int UPDATE_INTERVAL = 4;
+
+    std::vector<UserCommon_207177197_301251571::Position> runBFS(const UserCommon_207177197_301251571::Position& start, const UserCommon_207177197_301251571::Position& goal,
+                                   const std::vector<std::vector<char>>& grid);
+
+    UserCommon_207177197_301251571::Direction getDirectionTo(const UserCommon_207177197_301251571::Position& from, const UserCommon_207177197_301251571::Position& to) const;
+    ActionRequest rotateToward(UserCommon_207177197_301251571::Direction current, UserCommon_207177197_301251571::Direction target) const;
+
+    // Line-of-fire / obstacle helpers
+    bool isEnemyInLineOfFire(const UserCommon_207177197_301251571::Position& myPos,
+                                 const std::vector<std::vector<char>>& grid,
+                                 char enemySymbol) const;
+
+    bool isWallAhead(const UserCommon_207177197_301251571::Position& myPos,
+                         const std::vector<std::vector<char>>& grid) const;
+
+    // Helper to convert 4-direction to 8-direction approximation
+    UserCommon_207177197_301251571::Direction approximateTo8Direction(UserCommon_207177197_301251571::Direction dir4) const;
+
 };
 
 }
